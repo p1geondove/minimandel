531a4cb420856680f37d205b0419e44d380d9060
Leonard Peil <39673938+HelixFire@users.noreply.github.com>
2021-11-08 19:29:30 +0100

    minor changes again
    packed up some written out lines to one line
    uniform resolution when changes happen
    some more comments

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import pygame, sys, math
 from pygame.draw import rect
 from pygame.constants import KEYDOWN, K_ESCAPE, MOUSEBUTTONDOWN, MOUSEBUTTONUP, MOUSEMOTION, RESIZABLE
 
-Vadd = lambda a,b : (a[0]+b[0],a[1]+b[1])   #add/subtract/multiply/divide 2 tuples like numpy arrays
+Vadd = lambda a,b : (a[0]+b[0],a[1]+b[1])   #add/subtract/multiply/divide 2 tuples and/or lists like numpy arrays
 Vsub = lambda a,b : (a[0]-b[0],a[1]-b[1])
 Vmul = lambda a,b : (a[0]*b[0],a[1]*b[1])
 Vdiv = lambda a,b : (a[0]/b[0],a[1]/b[1])
@@ -19,12 +19,12 @@ def get_pixel_iters(constant, maxiter = 1000):  #in: complex, out: num of iterat
     return 0    #function bounces arround to much
 
 def hanlde_input(event, mpressed, viewoffset, scale, res):
-    if event.type == pygame.QUIT:   #pressing close button of the window
+    if event.type == pygame.QUIT:   #Exit pressing close button of the window
         pygame.quit()
         sys.exit()
     
     elif event.type == KEYDOWN:
-        if event.key == K_ESCAPE:   #exit windows using 'Esc' button
+        if event.key == K_ESCAPE:   #exit using 'Esc' button
             pygame.quit()
             sys.exit()
 
@@ -34,12 +34,12 @@ def hanlde_input(event, mpressed, viewoffset, scale, res):
         if event.button == 1: #left mousebutton
             mpressed = True
         elif event.button == 4: #zoom in
-            viewoffset = Vmul(viewoffset,(2,2)) #idk how this works but it does
-            viewoffset = Vsub(viewoffset,mpos)
+            viewoffset = Vmul(viewoffset,(2,2)) #double the zoom
+            viewoffset = Vsub(viewoffset,mpos)  #go middle to the mouse
             scale *= 2
         if event.button == 5: #zoom out
-            viewoffset = Vadd(viewoffset,mpos)
-            viewoffset = Vdiv(viewoffset,(2,2))
+            viewoffset = Vadd(viewoffset,mpos)  #go middle to the mouse
+            viewoffset = Vdiv(viewoffset,(2,2)) #half the zoom
             scale /= 2
         
     elif event.type == MOUSEBUTTONUP: #when you 'unpress' a button
@@ -55,7 +55,7 @@ def hanlde_input(event, mpressed, viewoffset, scale, res):
 
 def run():
     res = 10    #how big each pixel is (in actual pixel)
-    scale = 200 #the zoom level, 1 unit is {scale} pixel
+    scale = 50  #the zoom level, 1 unit is {scale} pixel
     winsize = (300,300)
     viewoffset = [winsize[0]/2,winsize[1]/2]
     mpressed = False
@@ -66,11 +66,9 @@ def run():
     clock = pygame.time.Clock()
 
     while True:
-        for y in range(0,winsize[1],res):       #for every virtual pixel (resolution)
+        for y in range(0,winsize[1],res):       #for every virtual pixel (resolution, not screen pixel)
             for x in range(0,winsize[0],res):
-                px = (x-viewoffset[0])/scale    #get complex value of pixel
-                py = (y-viewoffset[1])/scale
-                pixelpos = complex(px,py)       
+                pixelpos = complex(Vdiv(Vsub((x,y),viewoffset),[scale]*2))  #get complex value of pixel     
                 maxiter = math.log2(scale)**2     #"dynamic iterations" kinda works
                 color = get_pixel_iters(pixelpos,maxiter+100)*2%255 #amount of iterations % 255 in greyscale
                 rect(
@@ -82,9 +80,9 @@ def run():
             for event in pygame.event.get():    #yes i know this looks bad, but it works
                 mpressed,viewoffset,scale,res = hanlde_input(event,mpressed,viewoffset,scale,res)
                 winsize = win.get_size()
-            pygame.display.flip()
+            pygame.display.flip()   #update the window
 
-        clock.tick(30)
+        clock.tick(200)
         res -= res//2   #after every completed frame double the resolution
         if res < 1: res = 1
 
